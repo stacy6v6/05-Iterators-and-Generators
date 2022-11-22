70edf900d5497290c139eb65854fe4c1240645f3
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2022-11-22 11:12:39 +0800

    Generator implementation of Fibonacci

---
+++ generator.js
@@ -0,0 +1,27 @@
+function createFiboGenerator(num) {
+  let num1 = 0
+  let nextNum = 0
+  let num2 = 1
+  let count = 1
+
+  return function* generatorFiboFunction() {
+    while (count !== num) {
+      yield nextNum
+      nextNum = num1 + num2
+      num1 = num2
+      num2 = nextNum
+      count += 1
+    }
+    yield nextNum
+  }
+}
+
+const genFiboFunc1 = createFiboGenerator(4)
+const genFiboFuncObj1 = genFiboFunc1()
+console.log([...genFiboFuncObj1])
+
+const genFiboFunc2 = createFiboGenerator(4)
+const genFiboFuncObj2 = genFiboFunc2()
+for (let i = 0; i < 4; i += 1) {
+  console.log(genFiboFuncObj2.next())
+}
